9449d5ba641deb6a7cbee7a1d890f90796f8785b
Christian Berndt <berndt@inofix.ch>
2016-03-15 14:53:29 +0100

    Added getUrlTitle() method to ContactAssetRenderer.

--- contact-manager-portlet/src/main/java/ch/inofix/portlet/contact/asset/ContactAssetRenderer.java
+++ contact-manager-portlet/src/main/java/ch/inofix/portlet/contact/asset/ContactAssetRenderer.java
@@ -1,6 +1,8 @@
+
 package ch.inofix.portlet.contact.asset;
 
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
@@ -17,153 +19,186 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portlet.asset.model.BaseAssetRenderer;
 
 /**
- * 
  * @author Christian Berndt
  * @created 2015-05-19 17:25
- * @modified 2015-07-30 15:01
- * @version 1.0.7
- *
+ * @modified 2016-03-15 14:04
+ * @version 1.0.8
  */
 public class ContactAssetRenderer extends BaseAssetRenderer {
 
-	private static Log log = LogFactoryUtil.getLog(ContactAssetRenderer.class
-			.getName());
+    private static Log log =
+        LogFactoryUtil.getLog(ContactAssetRenderer.class.getName());
 
-	private Contact contact;
+    private Contact contact;
 
-	public ContactAssetRenderer(Contact contact) {
+    public ContactAssetRenderer(Contact contact) {
 
-		this.contact = contact;
-	}
+        this.contact = contact;
+    }
 
-	@Override
-	public String getClassName() {
-		return Contact.class.getName();
-	}
+    @Override
+    public String getClassName() {
 
-	@Override
-	public long getClassPK() {
-		return contact.getContactId();
-	}
+        return Contact.class.getName();
+    }
 
-	@Override
-	public long getGroupId() {
-		return contact.getGroupId();
-	}
+    @Override
+    public long getClassPK() {
 
-	@Override
-	public String getSummary(Locale locale) {
-		return contact.getFormattedName();
-	}
+        return contact.getContactId();
+    }
 
-	@Override
-	public String getTitle(Locale locale) {
-		return contact.getFullName(true);
-	}
+    @Override
+    public long getGroupId() {
 
-	@Override
-	public PortletURL getURLEdit(LiferayPortletRequest liferayPortletRequest,
-			LiferayPortletResponse liferayPortletResponse) throws Exception {
+        return contact.getGroupId();
+    }
 
-		PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(
-				getControlPanelPlid(liferayPortletRequest),
-				"contactmanager_WAR_contactmanager",
-				PortletRequest.ACTION_PHASE);
+    @Override
+    public String getSummary(Locale locale) {
 
-		String backURL = (String) liferayPortletRequest.getAttribute("backURL");
+        return contact.getFormattedName();
+    }
 
-		portletURL.setParameter("contactId",
-				String.valueOf(contact.getContactId()));
-		portletURL.setParameter("javax.portlet.action", "editContact");
-		portletURL.setParameter("mvcPath", "/html/edit_contact.jsp");
-		if (Validator.isNotNull(backURL)) {
-			portletURL.setParameter("backURL", backURL);
-		}
+    @Override
+    public String getTitle(Locale locale) {
 
-		return portletURL;
+        return contact.getFullName(true);
+    }
 
-	}
+    @Override
+    public PortletURL getURLEdit(
+        LiferayPortletRequest liferayPortletRequest,
+        LiferayPortletResponse liferayPortletResponse)
+        throws Exception {
 
-	@Override
-	public String getURLViewInContext(
-			LiferayPortletRequest liferayPortletRequest,
-			LiferayPortletResponse liferayPortletResponse,
-			String noSuchEntryRedirect) {
+        PortletURL portletURL =
+            liferayPortletResponse.createLiferayPortletURL(
+                getControlPanelPlid(liferayPortletRequest),
+                "contactmanager_WAR_contactmanager",
+                PortletRequest.ACTION_PHASE);
 
-		try {
-			PortletURL portletURL = liferayPortletResponse
-					.createActionURL("contactmanager_WAR_contactmanager");
+        String backURL = (String) liferayPortletRequest.getAttribute("backURL");
 
-			portletURL.setParameter("mvcPath", "/html/view_contact.jsp");
-			portletURL.setParameter("contactId",
-					String.valueOf(contact.getContactId()));
-			portletURL.setParameter("javax.portlet.action", "viewContact");
-			portletURL.setWindowState(WindowState.MAXIMIZED);
+        portletURL.setParameter(
+            "contactId", String.valueOf(contact.getContactId()));
+        portletURL.setParameter("javax.portlet.action", "editContact");
+        portletURL.setParameter("mvcPath", "/html/edit_contact.jsp");
+        if (Validator.isNotNull(backURL)) {
+            portletURL.setParameter("backURL", backURL);
+        }
 
-			return portletURL.toString();
-			
-		} catch (Exception e) {
-		}
+        return portletURL;
 
-		return null;
+    }
 
-	}
+    @Override
+    public String getUrlTitle() {
 
-	@Override
-	public long getUserId() {
-		return contact.getUserId();
-	}
+        StringBuilder sb = new StringBuilder();
 
-	@Override
-	public String getUserName() {
-		return contact.getUserName();
-	}
+        if (Validator.isNotNull(contact.getFullName(false))) {
+            sb.append(contact.getFullName(false).toLowerCase());
+        }
 
-	@Override
-	public String getUuid() {
-		return contact.getUuid();
-	}
+        String urlTitle = sb.toString();
 
-	@Override
-	public boolean hasEditPermission(PermissionChecker permissionChecker)
-			throws PortalException, SystemException {
+        Pattern _friendlyURLPattern = Pattern.compile("[^a-z0-9_-]");
 
-		return ContactPermission.contains(permissionChecker,
-				contact.getContactId(), ActionKeys.UPDATE);
-	}
+        urlTitle = FriendlyURLNormalizerUtil.normalize(urlTitle, _friendlyURLPattern);
 
-	@Override
-	public boolean hasViewPermission(PermissionChecker permissionChecker)
-			throws PortalException, SystemException {
+        return urlTitle;
 
-		return ContactPermission.contains(permissionChecker,
-				contact.getContactId(), ActionKeys.VIEW);
-	}
+    }
 
-	@Override
-	public boolean isPrintable() {
-		return true;
-	}
+    @Override
+    public String getURLViewInContext(
+        LiferayPortletRequest liferayPortletRequest,
+        LiferayPortletResponse liferayPortletResponse,
+        String noSuchEntryRedirect) {
 
-	@Override
-	public String render(RenderRequest renderRequest,
-			RenderResponse renderResponse, String template) throws Exception {
+        try {
+            PortletURL portletURL =
+                liferayPortletResponse.createActionURL("contactmanager_WAR_contactmanager");
 
-		if (template.equals(TEMPLATE_FULL_CONTENT)) {
+            portletURL.setParameter("mvcPath", "/html/view_contact.jsp");
+            portletURL.setParameter(
+                "contactId", String.valueOf(contact.getContactId()));
+            portletURL.setParameter("javax.portlet.action", "viewContact");
+            portletURL.setWindowState(WindowState.MAXIMIZED);
 
-			renderRequest.setAttribute("CONTACT", contact);
+            return portletURL.toString();
 
-			return "/html/asset/" + template + ".jsp";
+        }
+        catch (Exception e) {
+        }
 
-		} else {
-			return null;
-		}
-	}
+        return null;
 
+    }
+
+    @Override
+    public long getUserId() {
+
+        return contact.getUserId();
+    }
+
+    @Override
+    public String getUserName() {
+
+        return contact.getUserName();
+    }
+
+    @Override
+    public String getUuid() {
+
+        return contact.getUuid();
+    }
+
+    @Override
+    public boolean hasEditPermission(PermissionChecker permissionChecker)
+        throws PortalException, SystemException {
+
+        return ContactPermission.contains(
+            permissionChecker, contact.getContactId(), ActionKeys.UPDATE);
+    }
+
+    @Override
+    public boolean hasViewPermission(PermissionChecker permissionChecker)
+        throws PortalException, SystemException {
+
+        return ContactPermission.contains(
+            permissionChecker, contact.getContactId(), ActionKeys.VIEW);
+    }
+
+    @Override
+    public boolean isPrintable() {
+
+        return true;
+    }
+
+    @Override
+    public String render(
+        RenderRequest renderRequest, RenderResponse renderResponse,
+        String template)
+        throws Exception {
+
+        if (template.equals(TEMPLATE_FULL_CONTENT)) {
+
+            renderRequest.setAttribute("CONTACT", contact);
+
+            return "/html/asset/" + template + ".jsp";
+
+        }
+        else {
+            return null;
+        }
+    }
 }
