8fef996d38d34f425ad5609dbbebf341f8dab9ed
Christian Berndt <berndt@inofix.ch>
2016-03-22 21:24:50 +0100

    Enabled index based sorting.

--- timetracker-portlet/src/main/java/ch/inofix/portlet/timetracker/search/TaskRecordSearch.java
+++ timetracker-portlet/src/main/java/ch/inofix/portlet/timetracker/search/TaskRecordSearch.java
@@ -9,15 +9,13 @@ import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 
 import ch.inofix.portlet.timetracker.model.TaskRecord;
-import ch.inofix.portlet.timetracker.util.TaskRecordFields;
-import ch.inofix.portlet.timetracker.util.TimetrackerPortletKeys;
-import ch.inofix.portlet.timetracker.util.TimetrackerPortletUtil;
-
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portlet.PortalPreferences;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
 
 /**
  * @author Christian Berndt
@@ -37,25 +35,25 @@ public class TaskRecordSearch extends SearchContainer<TaskRecord> {
     static Map<String, String> orderableHeaders = new HashMap<String, String>();
 
     static {
-        headerNames.add(TaskRecordFields.TASK_RECORD_ID);
-        headerNames.add(TaskRecordFields.WORK_PACKAGE);
-        headerNames.add(TaskRecordFields.DESCRIPTION);
-        headerNames.add(TaskRecordFields.START_DATE);
-        headerNames.add(TaskRecordFields.END_DATE);
-        headerNames.add(TaskRecordFields.DURATION);
+        headerNames.add("create-date");
+        headerNames.add("duration");
+        headerNames.add("description");
+        headerNames.add("end-date");
+        headerNames.add("modified-date");
+        headerNames.add("start-date");
+        headerNames.add("task-record-id");
+        headerNames.add("ticket-url");
+        headerNames.add("work-package");
 
-        orderableHeaders.put(
-            TaskRecordFields.TASK_RECORD_ID, TaskRecordFields.TASK_RECORD_ID);
-        orderableHeaders.put(
-            TaskRecordFields.WORK_PACKAGE, TaskRecordFields.WORK_PACKAGE);
-        orderableHeaders.put(
-            TaskRecordFields.DESCRIPTION, TaskRecordFields.DESCRIPTION);
-        orderableHeaders.put(
-            TaskRecordFields.START_DATE, TaskRecordFields.START_DATE);
-        orderableHeaders.put(
-            TaskRecordFields.END_DATE, TaskRecordFields.END_DATE);
-        orderableHeaders.put(
-            TaskRecordFields.DURATION, TaskRecordFields.DURATION);
+        orderableHeaders.put("create-date", "create-date");
+        orderableHeaders.put("duration", "duration");
+        orderableHeaders.put("description", "description");
+        orderableHeaders.put("end-date", "end-date");
+        orderableHeaders.put("modified-date", "modified-date");
+        orderableHeaders.put("start-date", "start-date");
+        orderableHeaders.put("task-record-id", "task-record-id");
+        orderableHeaders.put("ticket-url", "ticket-url");
+        orderableHeaders.put("work-package", "work-package");
     }
 
     /**
@@ -90,25 +88,71 @@ public class TaskRecordSearch extends SearchContainer<TaskRecord> {
         super(portletRequest, new TaskRecordDisplayTerms(portletRequest), new TaskRecordSearchTerms(
             portletRequest), curParam, DEFAULT_DELTA, iteratorURL, headerNames, "there-are-no-results");
 
-        // Get the orderByCol and orderByType either from
-        // the request or from the portlet's preferences.
+        TaskRecordDisplayTerms displayTerms =
+            (TaskRecordDisplayTerms) getDisplayTerms();
+
+        iteratorURL.setParameter(
+            displayTerms.CREATE_DATE,
+            String.valueOf(displayTerms.getCreateDate()));
+        iteratorURL.setParameter(
+            displayTerms.DESCRIPTION,
+            String.valueOf(displayTerms.getDescription()));
+        iteratorURL.setParameter(
+            displayTerms.DURATION,
+            String.valueOf(displayTerms.getDescription()));
+        iteratorURL.setParameter(
+            displayTerms.END_DATE,
+            String.valueOf(displayTerms.getDescription()));
+        iteratorURL.setParameter(
+            displayTerms.MODIFIED_DATE,
+            String.valueOf(displayTerms.getDescription()));
+        iteratorURL.setParameter(
+            displayTerms.START_DATE,
+            String.valueOf(displayTerms.getDescription()));
+        iteratorURL.setParameter(
+            displayTerms.TASK_RECORD_ID,
+            String.valueOf(displayTerms.getDescription()));
+        iteratorURL.setParameter(
+            displayTerms.USER_ID, String.valueOf(displayTerms.getUserId()));
+        iteratorURL.setParameter(
+            displayTerms.USER_NAME, String.valueOf(displayTerms.getUserName()));
+        iteratorURL.setParameter(
+            displayTerms.WORK_PACKAGE,
+            String.valueOf(displayTerms.getWorkPackage()));
+
         try {
+            PortalPreferences preferences =
+                PortletPreferencesFactoryUtil.getPortalPreferences(portletRequest);
 
             String orderByCol =
-                ParamUtil.getString(
-                    portletRequest, TimetrackerPortletKeys.ORDER_BY_COL);
+                ParamUtil.getString(portletRequest, "orderByCol");
             String orderByType =
-                ParamUtil.getString(
-                    portletRequest, TimetrackerPortletKeys.ORDER_BY_TYPE);
+                ParamUtil.getString(portletRequest, "orderByType");
 
-            OrderByComparator obc =
-                TimetrackerPortletUtil.getOrderByComparator(
-                    orderByCol, orderByType);
+            if (Validator.isNotNull(orderByCol) &&
+                Validator.isNotNull(orderByType)) {
 
-            // Pass the orderBy parameter to the super class.
-            super.setOrderByCol(orderByCol);
-            super.setOrderByType(orderByType);
-            super.setOrderByComparator(obc);
+                preferences.setValue(
+                    "timetrackerportlerportlet_WAR_timetrackerportlerportlet",
+                    "timetracker-order-by-col", orderByCol);
+                preferences.setValue(
+                    "timetrackerportlerportlet_WAR_timetrackerportlerportlet",
+                    "timetracker-order-by-type", orderByType);
+            }
+            else {
+                orderByCol =
+                    preferences.getValue(
+                        "timetrackerportlerportlet_WAR_timetrackerportlerportlet",
+                        "timetracker-order-by-col", "create-date");
+                orderByType =
+                    preferences.getValue(
+                        "timetrackerportlerportlet_WAR_timetrackerportlerportlet",
+                        "timetracker-order-by-type", "desc");
+            }
+
+            setOrderableHeaders(orderableHeaders);
+            setOrderByCol(orderByCol);
+            setOrderByType(orderByType);
 
         }
         catch (Exception e) {
