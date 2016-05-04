0a35b6d5bed36be41870d287811bac6d7983fd8d
Christian Berndt <berndt@inofix.ch>
2016-05-04 22:49:15 +0200

    Implemented custom marker configuration.

--- map-portlet/src/main/java/ch/inofix/portlet/map/action/ConfigurationActionImpl.java
+++ map-portlet/src/main/java/ch/inofix/portlet/map/action/ConfigurationActionImpl.java
@@ -26,8 +26,8 @@ import com.liferay.portal.util.PortalUtil;
 /**
  * @author Christian Berndt
  * @created 2016-03-01 23:44
- * @modified 2016-03-26 16:09
- * @version 1.0.4
+ * @modified 2016-05-04 21:19
+ * @version 1.0.5
  */
 public class ConfigurationActionImpl extends DefaultConfigurationAction {
 
@@ -49,16 +49,12 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
 
         File file = uploadPortletRequest.getFile("file");
 
-        _log.info(file.getName());
-
         if (Validator.isNotNull(file) && file.length() > 0) {
 
             Reader in = new FileReader(file);
             Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
 
             for (CSVRecord record : records) {
-
-                _log.info(record);
 
                 labels.add(record.get(0));
 
@@ -70,7 +66,6 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
                 sb.append("]");
 
                 latLongs.add(sb.toString());
-
             }
         }
 
@@ -78,6 +73,8 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
         String mapCenter = ParamUtil.getString(actionRequest, "mapCenter");
         String mapHeight = ParamUtil.getString(actionRequest, "mapHeight");
         String mapZoom = ParamUtil.getString(actionRequest, "mapZoom");
+        String markerIconConfig =
+            ParamUtil.getString(actionRequest, "markerIconConfig");
         String[] markerLabels =
             ParamUtil.getParameterValues(actionRequest, "markerLabels");
         String[] markerLatLongs =
@@ -85,6 +82,7 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
         String tilesCopyright =
             ParamUtil.getString(actionRequest, "tilesCopyright");
         String tilesURL = ParamUtil.getString(actionRequest, "tilesURL");
+        String useDivIcon = ParamUtil.getString(actionRequest, "useDivIcon");
 
         // Append markers from file to markers from request
         markerLabels =
@@ -96,10 +94,12 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
         setPreference(actionRequest, "mapCenter", mapCenter);
         setPreference(actionRequest, "mapHeight", mapHeight);
         setPreference(actionRequest, "mapZoom", mapZoom);
+        setPreference(actionRequest, "markerIconConfig", markerIconConfig);
         setPreference(actionRequest, "markerLabels", markerLabels);
         setPreference(actionRequest, "markerLatLongs", markerLatLongs);
         setPreference(actionRequest, "tilesCopyright", tilesCopyright);
         setPreference(actionRequest, "tilesURL", tilesURL);
+        setPreference(actionRequest, "useDivIcon", useDivIcon);
 
         super.processAction(portletConfig, actionRequest, actionResponse);
 
