719d3404f74155e450669404119e4736915928a9
Christian Berndt <berndt@inofix.ch>
2016-10-15 22:39:46 +0200

    Send newsletter.

---
+++ newsletter-portlet/src/main/java/ch/inofix/portlet/newsletter/util/PortletKey.java
@@ -0,0 +1,17 @@
+package ch.inofix.portlet.newsletter.util;
+
+import com.liferay.portal.util.PortletKeys;
+
+/**
+ *
+ * @author Christian Berndt
+ * @created 2016-10-15 22:10
+ * @modified 2016-10-15 22:10
+ * @version 1.0.0
+ *
+ */
+public class PortletKey extends PortletKeys {
+
+    public static final String NEWSLETTER = "newsletter";
+
+}
