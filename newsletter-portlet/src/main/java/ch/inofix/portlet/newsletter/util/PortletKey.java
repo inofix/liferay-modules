1020bab867639921233b7f2f1e4880253eeaa2dd
Christian Berndt <berndt@inofix.ch>
2016-10-16 23:21:29 +0200

    Fixed PortletKey.

--- newsletter-portlet/src/main/java/ch/inofix/portlet/newsletter/util/PortletKey.java
+++ newsletter-portlet/src/main/java/ch/inofix/portlet/newsletter/util/PortletKey.java
@@ -6,12 +6,12 @@ import com.liferay.portal.util.PortletKeys;
  *
  * @author Christian Berndt
  * @created 2016-10-15 22:10
- * @modified 2016-10-15 22:10
- * @version 1.0.0
+ * @modified 2016-10-16 22:35
+ * @version 1.0.1
  *
  */
 public class PortletKey extends PortletKeys {
 
-    public static final String NEWSLETTER = "newsletter";
+    public static final String NEWSLETTER = "newsletterportlet_WAR_newsletterportlet";
 
 }
