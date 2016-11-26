2706f222b9c46b5e9e78a020d7481e71ed7f2de5
Christian Berndt <berndt@inofix.ch>
2016-11-26 23:33:18 +0100

    Removed deprecated timetracker-portlet from master-branch.

--- timetracker-portlet/src/main/java/ch/inofix/portlet/timetracker/util/TimeFormat.java
+++
@@ -1,9 +0,0 @@
-package ch.inofix.portlet.timetracker.util;
-
-public class TimeFormat {
-
-    public static final String DURATION = "duration";
-    public static final String FROM_UNTIL = "from-until";
-
-
-}
