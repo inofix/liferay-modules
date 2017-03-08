749644956eab0677fc795a12135e059cd3a05a8e
Christian Berndt <berndt@inofix.ch>
2017-03-08 19:55:12 +0100

    Service and model configuration.

---
+++ data-portlet/src/main/java/ch/inofix/portlet/data/service/impl/MeasurementLocalServiceImpl.java
@@ -0,0 +1,35 @@
+package ch.inofix.portlet.data.service.impl;
+
+import ch.inofix.portlet.data.service.base.MeasurementLocalServiceBaseImpl;
+
+/**
+ * The implementation of the measurement local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link ch.inofix.portlet.data.service.MeasurementLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
+ * </p>
+ *
+ * @author Christian Berndt
+ * @created 2017-03-08 19:46
+ * @modified 2017-03-08 19:46
+ * @version 1.0.0
+ * @see ch.inofix.portlet.data.service.base.MeasurementLocalServiceBaseImpl
+ * @see ch.inofix.portlet.data.service.MeasurementLocalServiceUtil
+ */
+public class MeasurementLocalServiceImpl extends
+        MeasurementLocalServiceBaseImpl {
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never reference this interface directly. Always use {@link
+     * ch.inofix.portlet.data.service.MeasurementLocalServiceUtil} to access the
+     * measurement local service.
+     */
+}
